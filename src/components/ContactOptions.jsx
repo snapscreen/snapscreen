f859772e2a1221c6b815aa920ab93eb16eae126c
Matthias Grieder <matthias.grieder@snapscreen.com>
2022-11-02 18:03:09 +0100

    Update ContactOptions.jsx

--- src/components/ContactOptions.jsx
+++ src/components/ContactOptions.jsx
@@ -8,28 +8,64 @@ export function ContactOptions() {
           <EmailIcon />
           <span className="sr-only">Email</span>
         </dt>
-        <dd>hello@snapscreen.com</dd>
+        <dd>
+          <a
+            href="mailto:hello@snapscreen.com"
+            className="hover:text-blue-600"
+            rel="noreferrer"
+            target="_blank"
+          >
+            hello@snapscreen.com
+          </a>
+        </dd>
       </div>
       <div className="flex gap-2 items-center">
         <dt>
           <LinkedinIcon />
           <span className="sr-only">LinkedIn</span>
         </dt>
-        <dd>company/Snapscreen</dd>
+        <dd>
+          <a
+            href="https://www.linkedin.com/company/Snapscreen"
+            className="hover:text-blue-600"
+            rel="noreferrer"
+            target="_blank"
+          >
+            company/Snapscreen
+          </a>
+        </dd>
       </div>
       <div className="flex gap-2 items-center">
         <dt>
           <TwitterIcon />
           <span className="sr-only">Twitter</span>
         </dt>
-        <dd>@Snapscreen_com</dd>
+        <dd>
+          <a
+            href="https://twitter.com/snapscreen_com"
+            className="hover:text-blue-600"
+            rel="noreferrer"
+            target="_blank"
+          >
+            @snapscreen_com
+          </a>
+        </dd>
       </div>
       <div className="flex gap-2 items-center">
         <dt>
           <GithubIcon />
           <span className="sr-only">Github</span>
         </dt>
-        <dd>snapscreen</dd>
+        <dd>
+          <a
+            href="https://github.com/snapscreen"
+            className="hover:text-blue-600"
+            rel="noreferrer"
+            target="_blank"
+          >
+            snapscreen
+          </a>
+        </dd>
       </div>
     </dl>
   )
